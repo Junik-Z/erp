144e7ee8b4dc5134ccfb165f3814f6a91766fba1
Junik <zjxpdq@163.com>
2025-04-10 11:38:01 +0800

    feat: 引用了音频采集及播放的类。

--- api/ai.js
+++ api/ai.js
@@ -11,13 +11,23 @@ export function getAskApi(data) {
   }, false, true);
 }
 
+/**
+ * @description 获取声音列表
+ */
+export function getVoicesListApi(data) {
+  return request({
+    url: "/ai/voices",
+    method: "get",
+    data,
+  }, false, true);
+}
 
 /**
  * @description 根据语音获取音频数据
  */
 export function getRecordService(data) {
   return request({
-    url: "/ai/recordService",
+    url: "/ai/record",
     method: "post",
     data,
     responseType: "arraybuffer",
